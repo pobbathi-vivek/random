1b76021e8bdb5a0e439b82c945bd86babdc50962
pobbathi-vivek <vivekpobbathi1111@gmail.com>
2025-11-27 01:06:51 +0530

    all are foe example

---
+++ hello.py
@@ -0,0 +1,37 @@
+
+# #string
+# # string = "Hi my name is Vivek"
+# # print(string)
+
+# first_name = "Pobbathi"
+# last_name = "Vivek"
+
+# full_name = first_name + " " + last_name
+# print(full_name)
+# len(full_name)
+
+# age = 18
+# can_vote = age >= 18
+# print(age > 15)
+
+
+# temp = 22
+# if temp >= 24: #hrll
+#     print("its hot!")
+# else:
+#     print("its nice weather!")
+
+
+# myset = set([12, 12, 23, 34, 65])
+
+
+class dog:
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+
+
+bittu = dog(name = "bitts", breed =  "retriever")
+
+
+
