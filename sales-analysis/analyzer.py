1e4744a2b347ceed2a36ae5c898c71db9882f0c3
pobbathi-vivek <vivekpobbathi1111@gmail.com>
2025-11-27 12:57:12 +0530

    yc

--- sales-analysis/analyzer.py
+++ sales-analysis/analyzer.py
@@ -35,7 +35,7 @@
 import pandas as pd
 from helper import calculate_total, format_currency
 
-# Read data  #this data i will read in the ithub
+# Read data  #this data i will read in
 df = pd.read_csv('data/sales.csv')
 
 # Calculate total for each row
